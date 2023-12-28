56e60a9efe310aa2f42da76e30b4f0ae5da5c157
Nada Amin <namin@seas.harvard.edu>
2023-12-28 08:34:21 +0100

    inc-state

--- barliman-tests.scm
+++ barliman-tests.scm
@@ -12,11 +12,11 @@
    (barliman-partial-programs
     3
     'append '(xs ys)
-    '(;;((append '() '()) ())
+    '(((append '() '()) ())
       ((append '(a) '(b)) (a b))
-      ;;((append '(g) '(h)) (g h))
+      ((append '(g) '(h)) (g h))
       ((append '(c d) '(e f)) (c d e f))
-      ;;((append '(w x y z) '(1 2 3 4)) (w x y z 1 2 3 4))
+      ((append '(w x y z) '(1 2 3 4)) (w x y z 1 2 3 4))
       ))
    )
   3)
