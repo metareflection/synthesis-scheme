04e6d8ccaf80bd28571fb17e08863b8441ed5388
Nada Amin <namin@seas.harvard.edu>
2023-12-30 16:24:35 +0100

    more experiments with threads and engines

--- threads.scm
+++ threads.scm
@@ -1,5 +1,8 @@
-;; inspired by https://web.archive.org/web/20170626072601/http://www.ccs.neu.edu/events/wand-symposium/talks/mitchfest-09-dybvig.pdf
-(define (parallel-exe n thunk)
+(load "load.scm")
+
+;; inspired by slide 10 of https://web.archive.org/web/20170626072601/http://www.ccs.neu.edu/events/wand-symposium/talks/mitchfest-09-dybvig.pdf
+
+(define (parallel-exe-no-engines n thunk)
   (let ((m (make-mutex))
         (c (make-condition))
         (done #f))
@@ -11,33 +14,71 @@
              (random-seed (+ 1000 (* 5 i)))
              (let ((result (thunk)))
                (with-mutex m
-                 (unless done
-                   (set! done result)
-                   (condition-signal c))))))
+                 (printf "now done ~a\n" i)
+                 (set! done result)
+                 (condition-signal c)))))
             (loop (+ i 1))))
-      (condition-wait c m))
-    done))
+      (condition-wait c m)
+      done)))
 
-(printf "//-exe 10\n")
-(time (parallel-exe 10 (lambda () (let loop ((i 0)) (if (= i 1000000000) 'done (loop (+ i 1))))))) ;; 1.8s
-(printf "//-exe 3\n")
-(time (parallel-exe 3  (lambda () (let loop ((i 0)) (if (= i 1000000000) 'done (loop (+ i 1))))))) ;; 1.4s
+(define (parallel-exe-engines n thunk)
+  (let ((m (make-mutex))
+        (c (make-condition))
+        (done #f))
+    (with-mutex m
+      (let loop ((i 0))
+        (when (< i n)
+          (fork-thread
+           (lambda ()
+             (random-seed (+ 10000 (* 5 i)))
+             (let eng-loop ((eng (make-engine thunk)))
+               (eng 800
+                    (lambda (ticks value)
+                      (with-mutex m
+                        (if done
+                            (printf "already done ~a\n" i)
+                            (begin
+                              (printf "done thanks to ~a\n" i)
+                              (set! done value)
+                              (condition-signal c)))))
+                    (lambda (new-eng)
+                      (if (with-mutex m done)
+                          (with-mutex m (printf "stopping engine ~a\n" i))
+                          (eng-loop new-eng)))))))
+          (loop (+ i 1))))
+      (condition-wait c m)
+      done)))
 
-#;
-(let ()
-  (define thunk
-    (lambda ()
-      (synthesize
-       'append 2 '(xs ys)
-       '(((append '() '()) ())
-         ((append '(a) '(b)) (a b))
-         ((append '(g) '(h)) (g h))
-         ((append '(c d) '(e f)) (c d e f))
-         ((append '(w x y z) '(1 2 3 4)) (w x y z 1 2 3 4))
+(define (test-// t parallel-exe)
+  (printf "//-exe 10 ~a\n" t)
+  (time (parallel-exe 10 (lambda () (let loop ((i 0)) (if (= i 100000000) 'done (loop (+ i 1))))))) ;; 1.8s
+  (printf "//-exe 3 ~a\n" t)
+  (time (parallel-exe 3  (lambda () (let loop ((i 0)) (if (= i 100000000) 'done (loop (+ i 1))))))) ;; 1.4s
+
+  (let ()
+    (define thunk
+      (lambda ()
+        (synthesize
+         'append 2 '(xs ys)
+         '(((append '() '()) ())
+           ((append '(a) '(b)) (a b))
+           ((append '(g) '(h)) (g h))
+           ((append '(c d) '(e f)) (c d e f))
+           ((append '(w x y z) '(1 2 3 4)) (w x y z 1 2 3 4))
+           )
+         '(expansion-count . 50000)
          )
-       '(expansion-count . 50000))
+        )
       )
-    )
-  (time-test
-   (parallel-exe 3 thunk)
-   '(((if (null? xs) ys (cons (car xs) (append (cdr xs) ys)))))))
+    (printf "//-3 for synthesizer ~a\n" t)
+    (time-test
+     (parallel-exe 3 thunk)
+     '(((if (null? xs) ys (cons (car xs) (append (cdr xs) ys))))))
+    (printf "//-10 for synthesizer ~a\n" t)
+    (time-test
+     (parallel-exe 10 thunk)
+     '(((if (null? xs) ys (cons (car xs) (append (cdr xs) ys)))))))
+  )
+
+(test-// "WITHOUT ENGINES" parallel-exe-no-engines)
+(test-// "WITH ENGINES" parallel-exe-engines)
